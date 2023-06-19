a11908b74e77dced35c96bc099d7bfb8cf5a5c53
Briankipr <brian.kiprono1@student.moringaschool.com>
2023-06-19 13:39:18 +0300

    phase-1-array-filter-method-lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution here
+// returns all drivers that match the passed in name;
+function findMatching(driver, name) {
+    return driver.filter(driver => driver.toLowerCase() === name.toLowerCase())
+  }
+//eturns matching drivers if case does not match but letters do
+  function fuzzyMatch(driver, letters) {
+    return driver.filter(driver => driver.startsWith(letters))
+  }
+  
+  function matchName(driver, name) {
+    return driver.filter(driver => driver.name === name)
+  }
+  
